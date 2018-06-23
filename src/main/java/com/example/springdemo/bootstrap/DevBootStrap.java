83ec8544fa9a63cccaa7ed6c3c0df270f509b489
hareesh94 <nutalapatihareesh@gmail.com>
2018-06-23 04:33:29 -0400

    Modified Publisher string to entity

--- src/main/java/com/example/springdemo/bootstrap/DevBootStrap.java
+++ src/main/java/com/example/springdemo/bootstrap/DevBootStrap.java
@@ -7,32 +7,39 @@ import org.springframework.stereotype.Component;
 
 import com.example.springdemo.model.Author;
 import com.example.springdemo.model.Book;
+import com.example.springdemo.model.Publisher;
 import com.example.springdemo.repositories.AuthorRepository;
 import com.example.springdemo.repositories.BookRepository;
+import com.example.springdemo.repositories.PublisherRepository;
 
 @Component
 public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> {
+	@Autowired
 	private AuthorRepository authorRepository;
+	@Autowired
 	private BookRepository bookRepository;
+	@Autowired
+	private PublisherRepository publisherRepository;
 	
-	public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
-		super();
-		this.authorRepository = authorRepository;
-		this.bookRepository = bookRepository;
-	}
+	
 
 	private void initData(){
 		Author Har = new Author("Har", "Nut");
-		Book aaa = new Book("Spring", "1234", "own");
+		Publisher pub1 = new Publisher("Own", "351 SW");
+		Book aaa = new Book("Spring", "1234", pub1);
 		Har.getBook().add(aaa);
 		aaa.getAuthors().add(Har);
+		publisherRepository.save(pub1);
+
 		authorRepository.save(Har);
 		bookRepository.save(aaa);
 		
 		Author Kal = new Author("Kal", "Tn");
-		Book bbb = new Book("Env", "1248", "Oneil");
+		Publisher pub2 = new Publisher("Oneil", "326 NE");
+		Book bbb = new Book("Env", "1248", pub2);
 		Kal.getBook().add(bbb);
 		bbb.getAuthors().add(Kal);
+		publisherRepository.save(pub2);
 		authorRepository.save(Kal);
 		bookRepository.save(bbb);
 	}
--- src/main/java/com/example/springdemo/model/Book.java
+++ src/main/java/com/example/springdemo/model/Book.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 @Entity
 public class Book {
 	@Id
@@ -18,19 +19,20 @@ public class Book {
 	private Long id;
 	private String title;
 	private String isbn;
-	private String publisher;
+	@OneToOne
+	private Publisher publisher;
 	@ManyToMany
 	@JoinTable(name="author_book", joinColumns=@JoinColumn(name="book_id"), inverseJoinColumns=@JoinColumn(name="author_id"))
 	private Set<Author> authors = new HashSet<>();
 	
 	
-	public Book(String title, String isbn, String publisher) {
+	public Book(String title, String isbn, Publisher publisher) {
 		super();
 		this.title = title;
 		this.isbn = isbn;
 		this.publisher = publisher;
 	}
-	public Book(String title, String isbn, String publisher, Set<Author> authors) {
+	public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
 		super();
 		this.title = title;
 		this.isbn = isbn;
@@ -55,10 +57,10 @@ public class Book {
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	public String getPublisher() {
+	public Publisher getPublisher() {
 		return publisher;
 	}
-	public void setPublisher(String publisher) {
+	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
 	public Set<Author> getAuthors() {
